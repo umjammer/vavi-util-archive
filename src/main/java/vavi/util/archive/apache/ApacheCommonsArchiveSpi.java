aceabcd0c57ffe4c9b9a2beef2ce74946434f491
Naohide Sano <namachapanda2005@gmail.com>
2022-03-09 21:21:22 +0900

    [apache-commons-compress] initial imports

---
+++ src/main/java/vavi/util/archive/apache/ApacheCommonsArchiveSpi.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright (c) 2021 by Naohide Sano, All rights reserved.
+ *
+ * Programmed by Naohide Sano
+ */
+
+package vavi.util.archive.apache;
+
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+import vavi.util.archive.Archive;
+import vavi.util.archive.spi.ArchiveSpi;
+
+
+/**
+ * The SPI for archives using Apache commons compress.
+ * 
+ * @author <a href="mailto:umjammer@gmail.com">Naohide Sano</a> (umjammer)
+ * @version 0.00 2021/11/16 umjammer initial version <br>
+ */
+public class ApacheCommonsArchiveSpi implements ArchiveSpi {
+
+    /**
+     * @param target currently accept {@link File} only.
+     */
+    @Override
+    public boolean canExtractInput(Object target) throws IOException {
+
+        if (!(target instanceof File)) {
+            throw new IllegalArgumentException("not supported type " + target);
+        }
+
+        InputStream is =
+            new BufferedInputStream(new FileInputStream((File) target));
+
+        // TODO accepts all
+        return true;
+    }
+
+    @Override
+    public Archive createArchiveInstance(Object obj) throws IOException {
+        return new ApacheCommonsArchive((File) obj);
+    }
+}
+
+/* */
---
+++ src/main/java/vavi/util/archive/apache/ApacheEntry.java
@@ -0,0 +1,126 @@
+/*
+ * Copyright (c) 2021 by Naohide Sano, All rights reserved.
+ *
+ * Programmed by Naohide Sano
+ */
+
+package vavi.util.archive.apache;
+
+import org.apache.commons.compress.archivers.ArchiveEntry;
+
+import vavi.util.archive.WrappedEntry;
+
+
+/**
+ * ApacheEntry.
+ *
+ * @author <a href="mailto:umjammer@gmail.com">Naohide Sano</a> (umjammer)
+ * @version 0.00 2021/11/17 umjammer initial version <br>
+ */
+public class ApacheEntry implements WrappedEntry<ArchiveEntry> {
+
+    private ArchiveEntry entry;
+
+    public ApacheEntry(ArchiveEntry entry) {
+        this.entry = entry;
+    }
+
+    @Override
+    public String getComment() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public long getCompressedSize() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public long getCrc() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public Object getExtra() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public int getMethod() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public String getName() {
+        return entry.getName();
+    }
+
+    @Override
+    public long getSize() {
+        return entry.getSize();
+    }
+
+    @Override
+    public long getTime() {
+        return entry.getLastModifiedDate().getTime();
+    }
+
+    @Override
+    public boolean isDirectory() {
+        return entry.isDirectory();
+    }
+
+    @Override
+    public void setComment(String comment) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void setCompressedSize(long csize) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void setCrc(long crc) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void setExtra(Object extra) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void setMethod(int method) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void setSize(long size) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void setTime(long time) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public ArchiveEntry getWrappedObject() {
+        return entry;
+    }
+}
+
+/* */
