5a251d369ca65883dee965488d70d2bbde505e78
Naohide Sano <namachapanda2005@gmail.com>
2022-06-20 16:22:08 +0900

    [binhex] fix magic detection

--- src/main/java/vavi/util/archive/binhex/BinhexInputStreamSpi.java
+++ src/main/java/vavi/util/archive/binhex/BinhexInputStreamSpi.java
@@ -9,9 +9,12 @@ package vavi.util.archive.binhex;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
+import java.util.logging.Level;
 
 import org.gjt.convert.binhex.BinHex4InputStream;
 
+import vavi.util.Debug;
+import vavi.util.StringUtil;
 import vavi.util.archive.spi.InputStreamSpi;
 
 
@@ -28,8 +31,8 @@ public class BinhexInputStreamSpi implements InputStreamSpi {
     private Object target;
 
     /** signature */
-    private static final String COMMENT =
-        "(This file must be converted with BinHex 4.0).";
+    private static final String COMMENT = "(This file must be converted with BinHex 4.0)";
+    private static final int L = COMMENT.length();
 
     /**
      * checks extractable or not.
@@ -49,15 +52,16 @@ public class BinhexInputStreamSpi implements InputStreamSpi {
             throw new IllegalArgumentException("cannot mark to stream");
         }
 
-        byte[] b = new byte[47];
+        byte[] b = new byte[L];
 
-        is.mark(47);
+        is.mark(L);
         int l = 0;
-        while (l < 47) {
-            l += is.read(b, l, 47 - l);
+        while (l < L) {
+            l += is.read(b, l, L - l);
         }
         is.reset();
 
+Debug.println(Level.FINE, "[" + new String(b, StandardCharsets.ISO_8859_1) + "], " + COMMENT.equals(new String(b, StandardCharsets.ISO_8859_1)) + "\n\n" + StringUtil.getDump(b));
         return COMMENT.equals(new String(b, StandardCharsets.ISO_8859_1));
     }
 
--- src/test/java/vavi/util/archive/binhex/BinhexInputStreamTest.java
+++ src/test/java/vavi/util/archive/binhex/BinhexInputStreamTest.java
@@ -6,14 +6,18 @@
 
 package vavi.util.archive.binhex;
 
+import java.io.File;
 import java.io.FileInputStream;
+import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
 import org.gjt.convert.binhex.BinHex4InputStream;
 import org.junit.jupiter.api.Test;
+import vavi.util.archive.Archives;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertInstanceOf;
 
 
 /**
@@ -31,6 +35,12 @@ class BinhexInputStreamTest {
 System.err.println(header.getDataLength());
         bis.close();
         assertEquals(137270, header.getDataLength());
+    }
+
+    @Test
+    void test2() throws Exception {
+        InputStream is = Archives.getInputStream(new File("src/test/resources/test.sit.hqx"));
+        assertInstanceOf(BinHex4InputStream.class, is);
     }
 }
 
